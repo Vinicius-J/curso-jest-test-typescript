8dbfeac25171f788e0e0ebf109fb3db7b67f3969
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-03-04 10:25:11 -0300

    Testando Product.ts

--- src/classes/product.ts
+++ src/classes/product.ts
@@ -1,11 +1,8 @@
 import type { CartItem } from './interfaces/cart-item';
 
 export class Product implements CartItem {
-  public name;
-  public price;
-
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
+  constructor(
+    public name: string,
+    public price: number,
+  ) {}
 }
