8837ef674dedaed35262fe7385487e4b2a30c7af
Vinícius Joaquim <viniciusjoaquimdossantos@gmail.com>
2026-03-04 10:03:31 -0300

    Primeiro teste com spyOn

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,13 +1,36 @@
-describe('TESTANDO ALGUMA COISA', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('TESTANDO OUTRA COISA', () => {
-  test('should return Vinícius', () => {
-    const nome = 'Vinícius';
-    expect(nome).toBe('Vinícius');
+describe('Persistency', () => {
+  // Depois de cada teste limpa todos os mocks
+  // Mocks são exemplos de uso
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // SUT - System UnderTtest
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    // Espião que vai olhar se o método foi chamado
+    // Primeiro o objeto e depois o método entre aspas
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
